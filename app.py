a40059475686ee43066d36dc1ec40c246eaabd91
K-0221 <ykmj0221@gmail.com>
2025-06-26 16:38:00 +0900

    first commit

--- app.py
+++ app.py
@@ -1,36 +1,29 @@
-from flask import Flask , request , jsonify, render_template,Response
 from flask_cors import CORS
+from flask import Flask, request, jsonify, render_template, Response
 import os
 import json
-
 app = Flask(__name__)
 CORS(app)
-
 @app.route('/')
 def home():
     return render_template('index.html')
-
-@app.route('/api/recipe',methods=['GET', 'POST'])
+@app.route('/api/recipe'methods=['GET'])
 def recipe():
-    if request.method == 'POST':
-        dish = request.form.get("dish")
-    else:
-        dish = request.args.get("dish")
+    dish = request.args.get("dish")
     if not dish:
-        return Response(
-            json.dumps({'error': 'No dish provided'},ensure_ascii=False).encode('utf-8'),
-            mimetype='application/json; charset=utf-8',
-            status=400
+        return  Response(
+    json.dumps({'error': 'No dish provided'},ensure_ascii=False),
+    content_type='application/json; charset=utf-8',
+    status=400
         )
     return Response(
-        json.dumps({
+    json.dumps({
             'dish': dish,
             'method': f'{dish}のおすすめ調理法はローストです！'
-        }, ensure_ascii=False).encode('utf-8'),
-        mimetype='application/json; charset=utf-8',
+        },ensure_ascii=False),
+        content_type='application/json; charset=utf-8',
         status=200
     )
-
-if __name__=='__main__':
-    port = int(os.environ.get("PORT",5000))
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
